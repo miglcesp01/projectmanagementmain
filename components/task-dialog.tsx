79297c6a4d5f98fc902805ad47aad9bb54470126
Miguel Licea <66740053+miglcesp01@users.noreply.github.com>
2025-03-26 18:27:54 +0100

    Update task-dialog.tsx

--- components/task-dialog.tsx
+++ components/task-dialog.tsx
@@ -353,7 +353,7 @@ export default function TaskDialog({ open, onOpenChange, project, task }: TaskDi
               </Select>
             </div>
           </div>
-          <div className="grid grid-cols-2 gap-4">
+          <div className="grid md:grid-cols-2 gap-4">
             <div className="space-y-2">
               <Label htmlFor="dueDate" className={errors.dueDate ? "text-destructive" : ""}>
                 Due Date
